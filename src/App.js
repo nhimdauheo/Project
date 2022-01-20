7dacab0fa0bfd8526da3e1e14d856d223bcbf66d
nhimdauheo <hieunguyen479@gmail.com>
2022-01-20 17:03:50 +0700

    RenderStaff

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import {Navbar, NavbarBrand} from 'reactstrap'
+import StaffMenu from './components/StaffComponents'
+import { STAFFS, DEPARTMENTS } from './data/staffs'
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+   
+    //Lấy dữ liệu từ file staffs
+    this.state = {
+      staffs : STAFFS
+    }
+  }
+  
+  render(){
+    return (
+    <div className="App">
+      <Navbar dark color="warning">
+        <div className='container'>
+          <NavbarBrand href='/'>Ứng dụng quản lý nhân sự V1.0</NavbarBrand>
+        </div>
+      </Navbar>
+      <StaffMenu staffs = {this.state.staffs}/>
+    </div>
+  );
+  }
+}
+export default App;
---
+++ src/components/StaffComponents.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Input, Form, Label } from "reactstrap"
+import dateFormat from "dateformat"
+
+class StaffMenu extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectStaffs: null
+        }
+    }
+    onStaffsSelect(staff) {
+        this.setState({ selectStaffs: staff })
+    }
+    renderStaff(staff) {
+        if (staff != null) {
+            return (
+                <Form style={{ border: "1px solid #ced4da", padding: 15, borderRadius: 10 }} className="col-md-6 col-lg-4">
+                    <h5><strong>Họ và tên: {staff.name}</strong></h5>
+                    <Label>Ngày sinh: {dateFormat(new Date(staff.doB), "dd/mm/yyyy")}</Label><br />
+                    <Label>Ngày vào công ty: {dateFormat(new Date(staff.startDate), "dd/mm/yyyy")}</Label><br />
+                    <Label>Phòng ban: {staff.department.name} </Label><br />
+                    <Label>Số ngày nghỉ còn lại: {staff.annualLeave}</Label><br />
+                    <Label>Số ngày đi làm thêm: {staff.overTime}</Label>
+                </Form>
+            )
+        } else {
+            return (
+                <div></div>
+            )
+        }
+    }
+
+    // Xử lý để hiện thị data trong file staffs.jsx
+    render() {
+        const staff = this.props.staffs.map((staff) => {
+            return (
+                    <div key={staff.id} className="col-md-6 col-lg-4 p-2">
+                        <Form >
+                            <Input style={{ cursor: "pointer", border: "1px solid #ced4da" }} onClick={() => this.onStaffsSelect(staff)} value={staff.name} />
+                        </Form>
+                    </div>
+            )
+        });
+        return (
+            <div className="container">
+                <div className="row">
+                    {staff}
+                </div><br />
+                <h5 style={{display: this.state.selectStaffs != null ?"none":"block"}}>Bấm vào nhân viên để xem thêm thông tin</h5>
+                <div className="row">
+                    {this.renderStaff(this.state.selectStaffs)}
+                </div>
+            </div>
+        )
+    }
+}
+export default StaffMenu;
