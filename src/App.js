9380a4a75b42cef70fdce82e57a8eae43ef34da3
nhimdauheo <hieunguyen479@gmail.com>
2022-01-21 10:09:51 +0700

    Final Assigment1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, {Component} from 'react'
-import {Navbar, NavbarBrand} from 'reactstrap'
+import React, { Component } from 'react'
+import { Navbar, NavbarBrand } from 'reactstrap'
 import StaffMenu from './components/StaffComponents'
 import { STAFFS } from './data/staffs'
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
-   
+
     //Lấy dữ liệu từ file staffs
     this.state = {
-      staffs : STAFFS
+      staffs: STAFFS
     }
   }
-  
-  render(){
-    return (
-    <div className="App">
-      <Navbar dark color="warning">
-        <div className='container'>
-          <NavbarBrand href='/'>Ứng dụng quản lý nhân sự V1.0</NavbarBrand>
-        </div>
-      </Navbar>
 
-      {/* Hiển thị các render trong StaffMenu */}
-      <StaffMenu staffs = {this.state.staffs}/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="warning">
+          <div className='container'>
+            <NavbarBrand href='/'>Ứng dụng quản lý nhân sự V1.0</NavbarBrand>
+          </div>
+        </Navbar>
+
+        {/* Hiển thị các render trong StaffMenu */}
+        <StaffMenu staffs={this.state.staffs} />
+      </div>
+    );
   }
 }
 export default App;
--- src/components/StaffComponents.js
+++ src/components/StaffComponents.js
@@ -41,13 +41,13 @@ class StaffMenu extends Component {
         // Sử dụng ArrayFunction để duyệt object
         const staff = this.props.staffs.map((staff) => {
             return (
-                    <div key={staff.id} className="col-md-6 col-lg-4 p-2">
-                        <Form >
+                <div key={staff.id} className="col-md-6 col-lg-4 p-2">
+                    <Form >
 
                         {/* Đổi thuộc tính trỏ chuột cursor: "pointer"  */}
-                            <Input style={{ cursor: "pointer", border: "1px solid #ced4da" }} onClick={() => this.onStaffsSelect(staff)} value={staff.name} />
-                        </Form>
-                    </div>
+                        <Input style={{ cursor: "pointer", border: "1px solid #ced4da" }} onClick={() => this.onStaffsSelect(staff)} value={staff.name} />
+                    </Form>
+                </div>
             )
         });
         return (
@@ -59,7 +59,7 @@ class StaffMenu extends Component {
                 </div><br />
 
                 {/* Điều kiện ẩn hiện h5 */}
-                <h5 style={{display: this.state.selectStaffs != null ?"none":"block"}}>Bấm vào nhân viên để xem thêm thông tin</h5>
+                <h5 style={{ display: this.state.selectStaffs != null ? "none" : "block" }}>Bấm vào nhân viên để xem thêm thông tin</h5>
 
                 {/* Hiển thị renderStaffSelected */}
                 <div className="row">
