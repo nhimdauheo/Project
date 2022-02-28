f93e7ec7b923385dd0f9eba1521e1cffe7fedc4e
nhimdauheo <hieunguyen479@gmail.com>
2022-02-28 14:48:20 +0700

    fix staff interface

--- src/components/StaffComponents.js
+++ src/components/StaffComponents.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { CardImg, Card, CardTitle } from "reactstrap"
+import { CardImg, Card, CardTitle, Input, Button, Form, FormGroup, Label, Col } from "reactstrap"
 import { Link } from 'react-router-dom'
 
 class StaffMenu extends Component {
     constructor(props) {
         super(props);
+
     }
 
     // Xử lý để hiện thị data trong file staffs.jsx
@@ -13,7 +14,7 @@ class StaffMenu extends Component {
         // Sử dụng ArrayFunction để duyệt object
         const staff = this.props.staffs.map((staff) => {
             return (
-                <div key={staff.id} className="col-6 col-sm-6 col-md-3 col-lg-2 p-2">
+                <div key={staff.id} className="col-6 col-sm-6 col-md-4 col-lg-2 p-2">
                     <Card inverse color="info">
                         <Link to={`/nhanvien/${staff.id}/`}>
                             <CardImg width="100%" src={staff.image} alt={staff.name} />
@@ -26,8 +27,22 @@ class StaffMenu extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-12">
-                        <h3><i class="fa fa-user-circle" aria-hidden="true"></i> Nhân viên</h3>
+                    <div className="col-12 m-1">
+                        <div className="row">
+                            <div className="col-6">
+                                <h3><i class="fa fa-user-circle" aria-hidden="true"></i> Nhân viên</h3>
+                            </div>
+                            <div className="col-6">
+                                <Form>
+                                    <FormGroup row>
+                                        <Col sm={8}>
+                                            <Input type="text" name="search" id="search"/>
+                                        </Col>
+                                      <Button color="primary">Tìm</Button>
+                                    </FormGroup>
+                                </Form>
+                            </div>
+                        </div>
                         <hr />
                     </div>
                 </div>
