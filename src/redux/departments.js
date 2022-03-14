026ada9a2adc7bc5375f78c004181c0a4a437e58
nhimdauheo <hieunguyen479@gmail.com>
2022-03-14 13:56:52 +0700

    Combining Reducers

---
+++ src/redux/departments.js
@@ -0,0 +1,8 @@
+import {DEPARTMENTS} from '../data/staffs'
+
+export const Departments = (state = DEPARTMENTS, action)=>{
+    switch (action.type){
+        default:
+            return state
+    }
+} 
---
+++ src/redux/staffs.js
@@ -0,0 +1,8 @@
+import {STAFFS} from '../data/staffs'
+
+export const Staffs = (state = STAFFS, action) => {
+    switch (action.type){
+        default: 
+            return state
+    }
+}
