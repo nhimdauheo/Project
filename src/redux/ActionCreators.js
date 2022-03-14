5fe83754cce835346914fd877ccb2d72f0a2b77c
nhimdauheo <hieunguyen479@gmail.com>
2022-03-14 16:00:08 +0700

    create AddStaff

---
+++ src/redux/ActionCreators.js
@@ -0,0 +1,14 @@
+import * as ActionTypes from './ActionTypes'
+
+export const addStaff = ( name, doB, startDate, department, salaryScale, annualLeave, overTime) => ({
+    type: ActionTypes.ADD_STAFF,
+    payload: {
+        name: name,
+        doB: doB,
+        startDate: startDate,
+        department: department,
+        salaryScale: salaryScale,
+        annualLeave: annualLeave,
+        overTime: overTime
+    }
+})
--- src/redux/staffs.js
+++ src/redux/staffs.js
@@ -1,7 +1,15 @@
 import {STAFFS} from '../data/staffs'
+import * as ActionTypes from './ActionTypes';
 
 export const Staffs = (state = STAFFS, action) => {
     switch (action.type){
+        case ActionTypes.ADD_STAFF:
+            var staff = action.payload;
+            staff.id = state.length;
+            staff.doB = new Date().toISOString();
+            staff.startDate = new Date().toISOString(); 
+            console.log("Staff: ", staff);
+            return state.concat(staff)
         default: 
             return state
     }
